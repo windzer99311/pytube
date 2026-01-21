9567afc8c715f07d410ce7e74a260638cef39e50
windzer99311 <windzergamer@gmail.com>
2026-01-21 19:31:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from fastapi import FastAPI
 import requests
 
 app = FastAPI()
+
 @app.get("/search")
 def get_stream(url: str):
     api = "https://api.vidssave.com/api/contentsite_api/media/parse"
+
+    headers = {
+        "accept": "*/*",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "origin": "https://vidssave.com",
+        "referer": "https://vidssave.com/",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    }
 
     data = {
         "auth": "20250901majwlqo",
         "domain": "api-ak.vidssave.com",
         "origin": "source",
-        "link": url,
+        "link": url
     }
 
-    r = requests.post(api, data=data)
+    r = requests.post(api, headers=headers, data=data, timeout=15)
+
     return r.text
