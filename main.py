e56ebef578e95555aadb0b1673df965976b1ed3d
windzer99311 <windzergamer@gmail.com>
2026-01-22 22:16:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 def main(context):
     # Log some info to the Appwrite console
-    context.log("Hello, Appwrite! The function has started.")
-
-    # Check if the request is a GET or POST
-    method = context.req.method
-    
-    # Get data from the request body (if provided)
-    payload = context.req.body or {}
-
-    # Logic: Return a simple greeting
-    name = payload.get("name", "Guest")
+    from pytubefix import YouTube
+    url="https://www.youtube.com/watch?v=Jtauh8GcxBY"
+    yt=YouTube(url).streams.first().url
+    print(yt)
     
     return context.res.json({
-        "message": f"Hello {name}!",
+        "message": f"Hello {yt}!",
         "method": method,
         "status": "success"
     })
